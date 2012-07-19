d9d41f212e4d01dc8ffdeeffa83f8ffec0cd40e8
Chad Hahn <chad@codegoop.com>
2012-07-19 08:37:53 -0400

    Removed useless code

--- src/main/java/com/codegoop/init/AppConfig.java
+++ src/main/java/com/codegoop/init/AppConfig.java
@@ -7,15 +7,4 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.codegoop")
-public class AppConfig {
-
-//  @Bean
-//  public ContentNegotiatingViewResolver contentNegotiatingViewResolver() {
-//    final ContentNegotiatingViewResolver viewResolver = new ContentNegotiatingViewResolver();
-//    viewResolver.setDefaultContentType(MediaType.APPLICATION_JSON);
-//    final List<View> defaultViews = new ArrayList<View>();
-//    defaultViews.add(new MappingJacksonJsonView());
-//    viewResolver.setDefaultViews(defaultViews);
-//    return viewResolver;
-//  }
-}
+public class AppConfig {}
